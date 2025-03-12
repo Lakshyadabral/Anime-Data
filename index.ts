c1b57001ca0494e3dc3d2a2c0ab341154eb7a1dd
Lakshyadabral <lakshyadabral24554@gmail.com>
2025-03-12 20:34:39 -0300

    cors fixed

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import { Hono } from 'hono';
 import { serve } from 'bun';
 import animeRoutes from './routes/animeRoutes';
+import { cors } from "hono/cors";
 
 
 const app = new Hono();
 
 
-
+app.use(
+  cors({
+    origin: "*", // Allow all origins (or specify only frontend URL)
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    allowHeaders: ["Content-Type"],
+  })
+);
 
 // Register routes
 app.route('/animes', animeRoutes);
