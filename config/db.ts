ce1e66a0008a196bb3596acc5e98379aa2cfbd6b
Lakshyadabral <lakshyadabral24554@gmail.com>
2025-03-12 19:12:06 -0300

    Completed

--- config/db.ts
+++ config/db.ts
@@ -1,16 +1,17 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
+// Use environment variables OR hardcoded URI
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://lakshyadabral24554:Vegito@cluster0.zprtv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
 const client = new MongoClient(MONGO_URI);
 
 try {
-  await client.connect(); // Connect to MongoDB
-  console.log('Connected to MongoDB');
+  await client.connect();
+  console.log(" Connected to MongoDB Atlas");
 } catch (error) {
-  console.error('Failed to connect to MongoDB:', error);
-  process.exit(1); // Exit the process if the connection fails
+  console.error(" Failed to connect to MongoDB:", error);
+  process.exit(1);
 }
 
-export const db = client.db('AnimeData'); // Use the AnimeData database
-export const animeCollection = db.collection('AnimeData'); 
+export const db = client.db("animeDB"); 
+export const animeCollection = db.collection("anime"); 
